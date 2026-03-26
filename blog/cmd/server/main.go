82bd6c7948b203a5d9aabeb9810c0528d119825f
SaviolaX <withcraft1717@gmail.com>
2026-03-26 17:58:01 +0200

    feat: implement main server entry point for blog application with routing setup

---
+++ blog/cmd/server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/SaviolaX/blog/internal/config"
+	"github.com/SaviolaX/blog/internal/db"
+	"github.com/SaviolaX/blog/internal/handler"
+	"github.com/SaviolaX/blog/internal/repository"
+	"github.com/SaviolaX/blog/internal/service"
+)
+
+func main() {
+	cfg := config.Load()
+
+	database := db.Connect(cfg.DB)
+	db.Migrate(database)
+
+	userRepo := repository.NewUserRepository(database)
+	postRepo := repository.NewPostRepository(database)
+
+	userService := service.NewUserService(userRepo)
+	postService := service.NewPostService(postRepo)
+
+	userHandler := handler.NewUserHandler(userService)
+	postHandler := handler.NewPostHandler(postService)
+
+	r := handler.SetupRouter(userHandler, postHandler)
+
+	r.Run(cfg.App.Port)
+}
